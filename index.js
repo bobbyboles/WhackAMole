d20d20f3b85434dc36dcd705e1575edaabc03494
Bob Boles <robert.dale.boles@gmail.com>
2022-10-16 17:58:16 -0600

    added comments and documentation for known bug

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// setting variables for classes
+
 const container = document.querySelector('.container')
 const score = document.querySelector('.score')
 const timer = document.querySelector('.countdown')
 const span = document.querySelector(".close")
 const modal = document.querySelector(".modal")
 const splash = document.querySelector('.endSplash')
+
+// setting variables for sounds
+
 const themeSong = new Audio('/sounds/star-wars-theme-song.mp3')
 const vaderNo = new Audio('/sounds/no.mp3')
 const vader = new Audio('/sounds/vaderIntro.mp3')
@@ -14,8 +19,15 @@ const r2d2 = new Audio('/sounds/r2d2.mp3')
 const battle = new Audio('/sounds/battle_sfx.wav')
 const chewy = new Audio('/sounds/chewy.wav')
 
+// playing the theme song and vaders intro on load
+
 themeSong.play()
 vader.play()
+
+
+/*Function creates two random numbers, one for the random space class
+and the other for the random fighter spawn and assigns the random fighter to the random 
+space class*/
 
 function randomFighters() {
   const randomNum = Math.floor(Math.random() * 9 + 1)
@@ -32,7 +44,12 @@ function randomFighters() {
   }
 }
 
+// Interval for randomFighters so there is always fighters to shoot
+
 const intervalAdd = setInterval(randomFighters, 250)
+
+/* Function creates a random number and assigns it to a random space class
+then sets the source of the image to an empty string*/
 
 function removeFighters() {
   const randomNum = Math.floor(Math.random() * 9 + 1)
@@ -40,13 +57,27 @@ function removeFighters() {
   randomSpace.src = ''
 }
 
+//Interval for removing fighters so fighters pop in and out of the game
+
 const intervalRemove = setInterval(removeFighters, 400)
 
-function delayedHole(){
-}
+/*Count is global for Modal later in the script*/
 
 var count = 0
+
+/*Listener on the container of fighters with control flow dictating what happens when each
+fighter is clicked. Vader has increased points, Xwing Decreases points, pew sounds are played
+on each click and created inside the control flow so that they are played reguardless if the previous
+pew is finished. At the end the score is reflected in  the score class*/
+
+/*There is a bug where if e.taget.baseURI ends with .html when its locally hosted, this will not work and 
+must be reloaded until e.target.baseURI ends with the port. It's pretty infrequent but I'm going to submit with this bug.
+The way around it would be to find a different target in the click event*/
+
 container.addEventListener('click', function (e) {
+  console.log(e)
+  console.log(e.target.attributes[1])
+  console.log(e.target.src)
   if (e.target.src === `${e.target.baseURI}images/tie_fighter1.webp`) {
     e.target.src = ``
     count++
@@ -77,6 +108,10 @@ container.addEventListener('click', function (e) {
   score.textContent = `Score: ${count}`
 })
 
+/*20 second timer with sounds played at certain intervals to increase immersion
+at zero seconds the modal is displayed with final score as well as pausing the main 
+theme song and clearing the countdown so it stops*/
+
 let timeleft = 20;
 let countdown = setInterval(function () {
   if (timeleft <= 0) {
@@ -106,6 +141,8 @@ let countdown = setInterval(function () {
   }
   timeleft -= 1;
 }, 1000);
+
+//reloads the game upon hitting the x on the modal and rehides the modal
 
 span.onclick = function () {
   modal.style.display = "none";
